bd1906f9603df2836c160c425d0eef568b25fb95
Alex Kempton <alex@funwithtriangles.net>
2020-11-02 21:53:16 +0100

    Main logic in place

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,7 @@
 const paths = require('./paths')
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -21,17 +21,17 @@ module.exports = {
     new CleanWebpackPlugin(),
 
     // Copies files from target to destination folder
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: paths.public,
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-        },
-      ],
-    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: paths.public,
+    //       to: 'assets',
+    //       globOptions: {
+    //         ignore: ['*.DS_Store']
+    //       }
+    //     }
+    //   ]
+    // }),
 
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
@@ -47,24 +47,24 @@ module.exports = {
   module: {
     rules: [
       // JavaScript: Use Babel to transpile JavaScript files
-      {test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
 
       // Styles: Inject CSS into the head with source maps
       {
         test: /\.(scss|css)$/,
         use: [
           'style-loader',
-          {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1}},
-          {loader: 'postcss-loader', options: {sourceMap: true}},
-          {loader: 'sass-loader', options: {sourceMap: true}},
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
 
       // Images: Copy image files to build folder
-      {test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource'},
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
 
       // Fonts and SVGs: Inline files
-      {test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline'},
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
     ],
   },
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,77 @@
-// Test import of a JavaScript function
-import {example} from './js/example'
-
-// Test import of an asset
-import webpackLogo from './images/webpack-logo.svg'
-
-// Test import of styles
 import './styles/index.scss'
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+const board = document.querySelector('.board')
 
-const heading = document.createElement('h1')
-heading.textContent = example()
+const rowSize = 8
+const numCells = Math.pow(rowSize, 2)
 
-const app = document.querySelector('#root')
-app.append(logo, heading)
+let cells = []
+let keyIndex = 0
+
+// Create chessboard in HTML
+const cellWidth = 80 / rowSize
+board.style.gridTemplateRows = `repeat(${rowSize}, [row] ${cellWidth}vw)`
+board.style.gridTemplateColumns = `repeat(${rowSize}, [col] ${cellWidth}vw)`
+
+for (let i = 0; i < numCells; i++) {
+  const cell = document.createElement('div')
+  cell.classList.add('cell')
+  cell.addEventListener('click', () => {
+    cells[i] = !cells[i]
+    update()
+  })
+  board.append(cell)
+}
+
+const randomizeCells = () => {
+  cells = []
+  for (let i = 0; i < numCells; i++) {
+    cells[i] = Math.random() > 0.5
+  }
+}
+
+const randomizeKey = () => {
+  keyIndex = Math.floor(Math.random() * numCells)
+}
+
+const decodeCells = () => {
+  // Find secret index of warden's key by doing XOR on all indexes with true cells (heads)
+  let acc = 0
+
+  for (let i = 1; i < numCells; i++) {
+    if (cells[i] === true) {
+      acc ^= i
+    }
+  }
+
+  return acc
+}
+
+const getSolution = (currDecode, targetDecode) => currDecode ^ targetDecode
+
+const updateBoard = ({ decodedIndex, secretIndex, solutionIndex } = {}) => {
+  document.querySelectorAll('.cell').forEach((cell, i) => {
+    // Give cells heads/tails label
+    cell.textContent = cells[i] === true ? 'H' : 'T'
+
+    // Style cells accordingly
+    cell.classList.toggle('hasDecoded', i === decodedIndex)
+    cell.classList.toggle('hasKey', i === secretIndex)
+    cell.classList.toggle('shouldFlip', i === solutionIndex)
+  })
+}
+
+randomizeKey()
+randomizeCells()
+
+const update = () => {
+  const decodedIndex = decodeCells()
+
+  updateBoard({
+    decodedIndex,
+    secretIndex: keyIndex,
+    solutionIndex: getSolution(decodedIndex, keyIndex),
+  })
+}
+
+update()
